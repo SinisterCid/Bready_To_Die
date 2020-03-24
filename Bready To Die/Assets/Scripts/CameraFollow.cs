2c6da621e35df5c9acba7e8c746df12848d99248
nBieri10 <59703208+nBieri10@users.noreply.github.com>
2020-03-24 17:01:06 -0400

    Added basic character movement

---
+++ Bready To Die/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Camera))]
+public class CameraFollow : MonoBehaviour
+{
+
+    private Camera mainCam;
+    public List<Transform> targets;
+    public float smoothTime = 0.5f;
+    public Vector3 offset;
+
+    private Vector3 velocity;
+    public float minZoom;
+    public float maxZoom;
+    public float zoomLimiter = 50;
+
+    Vector3 magnitude = new Vector3(1,1,0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = GetComponent<Camera>();
+        foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            targets.Add(player.GetComponent<Transform>());
+        }
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (targets.Count == 0)
+        {
+            return;
+        }
+
+        Move();
+        Zoom();
+    }
+
+    private void Move()
+    {
+        Vector3 centerPoint = GetCenterPoint();
+        //Vector3 newPosition = centerPoint + offset;
+        transform.LookAt(centerPoint);
+        //transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+    }
+
+    private void Zoom()
+    {
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance()/ zoomLimiter);
+        mainCam.orthographicSize = Mathf.Lerp(mainCam.orthographicSize, newZoom, Time.deltaTime);
+    }
+
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.size.x;
+    }
+
+    Vector3 GetCenterPoint()
+    {
+        if (targets.Count == 1)
+        {
+            return targets[0].position;
+        }
+
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.center;
+    }
+
+    /*public void ShakeCamera()
+    {
+        iTween.ShakePosition(this.gameObject, magnitude, 0.5f);
+    }*/
+}
---
+++ Bready To Die/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance;
+    public LayerMask groundMask;
+
+    float currentX;
+    float currentZ;
+
+    public bool isGrounded;
+
+    CharacterController controller;
+
+    Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        MovePlayer();
+        Jump();
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        /*if (isGrounded)
+        {
+            GetAlignment();
+        }
+        else
+        {
+            transform.up = Vector3.up;
+        }*/
+    }
+
+    void MovePlayer()
+    {
+        float xRaw = Input.GetAxisRaw("Horizontal");
+        float zRaw = Input.GetAxisRaw("Vertical");
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 lookDirection = new Vector3(xRaw, 0.0f, zRaw);
+        if (xRaw != 0 || zRaw != 0)
+        {
+            transform.rotation = Quaternion.LookRotation(lookDirection);
+        }
+
+        Vector3 move = new Vector3(x, 0, z);
+        controller.Move(move * speed * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+
+    void GetAlignment()
+    {
+        RaycastHit hit;
+        Physics.Raycast(transform.position, -transform.up, out hit, 3f, groundMask);
+
+        Vector3 newUp = hit.normal;
+        transform.up = newUp;
+    }
+}
