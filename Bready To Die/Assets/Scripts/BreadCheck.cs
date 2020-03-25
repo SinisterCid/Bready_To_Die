a18eea57b9d9de66d7c44dc9bbe877ea8f654d8a
NolanGrant <41460773+NolanGrant@users.noreply.github.com>
2020-03-25 15:19:43 -0400

    eat the bread

---
+++ Bready To Die/Assets/Scripts/BreadCheck.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreadCheck : MonoBehaviour
+{
+    public Duck myDuck;
+    public GameObject mouth;
+    public float eatingDuration = 0.5f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(EatBread(other.gameObject));
+        Destroy(other.gameObject.GetComponent<Rigidbody>());  
+    }
+
+    IEnumerator EatBread(GameObject targetbread)
+    {
+        float t = eatingDuration;
+        Vector3 startPosition = targetbread.transform.position;
+        while (t > 0)
+        {
+            targetbread.transform.position = Vector3.Lerp(mouth.transform.position, startPosition, t / eatingDuration);
+            targetbread.transform.localScale = Vector3.one * Mathf.Lerp(0, 1, t / eatingDuration);
+            t -= Time.deltaTime;
+            yield return null;
+        }
+        myDuck.breadCount += 1;
+        Destroy(targetbread);
+        yield break;
+    }
+}
