0317d033fbc2010caf977f5b2f5ed336bf16e4e2
Marfoire <44646509+Marfoire@users.noreply.github.com>
2020-04-02 21:23:12 -0400

    Duck select scene, has start game and player adding/removing

---
+++ Bready To Die/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerManager : MonoBehaviour
+{
+    public List<GameObject> players;
+    public List<int> IDs;
+
+    public bool P1Exists = false;
+    public bool P2Exists = false;
+    public bool P3Exists = false;
+    public bool P4Exists = false;
+
+    public static PlayerManager playerManager;
+
+    public bool markerOnMouse;
+
+    private void SetInstance()
+    {
+        if (playerManager == null)
+        {
+            playerManager = this;
+            if (Application.isPlaying)
+            {
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Awake()
+    {
+        SetInstance();
+    }
+
+    public void AddPlayer(int ID)
+    {
+        if (!IDs.Contains(ID))
+        {
+            IDs.Add(ID);
+            if(ID == 0)
+            {
+                P1Exists = true;              
+            }
+            else if(ID == 1)
+            {
+                P2Exists = true;
+            }
+            else if (ID == 2)
+            {
+                P3Exists = true;
+            }
+            else if (ID == 3)
+            {
+                P4Exists = true;
+            }
+        }
+    }
+
+    public void RemovePlayer(int ID)
+    {
+        if (IDs.Contains(ID))
+        {
+            IDs.RemoveAll(x => x == ID);
+            if (ID == 0)
+            {
+                P1Exists = false;
+            }
+            else if (ID == 1)
+            {
+                P2Exists = false;
+            }
+            else if (ID == 2)
+            {
+                P3Exists = false;
+            }
+            else if (ID == 3)
+            {
+                P4Exists = false;
+            }
+        }
+    }
+
+
+    public void StartGameplayScene()
+    {
+        SceneManager.LoadScene("Nolan", LoadSceneMode.Single);
+    }
+
+
+}
---
+++ Bready To Die/Assets/Scripts/PlayerMarker.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PlayerMarker : MonoBehaviour
+{
+    public bool followingMouse;
+
+    private void OnDisable()
+    {
+        StopMouseFollow();
+    }
+
+    public void FollowMouse()
+    {
+        if (followingMouse != true && PlayerManager.playerManager.markerOnMouse != true)
+        {
+            followingMouse = true;
+            PlayerManager.playerManager.markerOnMouse = true;
+            
+        }
+        else if(followingMouse)
+        {
+            StopMouseFollow();
+        }
+    }
+
+    public void StopMouseFollow()
+    {
+            followingMouse = false;
+            PlayerManager.playerManager.markerOnMouse = false;
+    }
+
+    private void Update()
+    {
+        if (followingMouse)
+        {
+            transform.position = Input.mousePosition;
+        }
+    }
+}
