506f98183bd364c7a6970037e259ba29ed2de584
NolanGrant <41460773+NolanGrant@users.noreply.github.com>
2020-04-07 18:02:28 -0400

    timer, optimizations, cleanup

---
+++ Bready To Die/Assets/Scripts/GoToTitle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GoToTitle : MonoBehaviour
+{
+    public Text timerText;
+    public float timer = 120;
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+
+        timerText.text = Mathf.RoundToInt(timer).ToString();
+        if (timer <= 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
